ef2c3aa3bb76189ed3f27033c6475ed7ae5a40ac
johnfeshuk <johnfesh717@gmail.com>
2020-03-17 16:39:07 -0400

    Implemented feature 6

--- src/index.js
+++ src/index.js
@@ -114,10 +114,14 @@ class Game extends React.Component {
         }
 
         let status;
-        if (winner.winner != null) {
+        if (winner.winner) {
             status = "Winner: " + winner.winner;
         } else {
-            status = "Next player: " + (this.state.xIsNext ? 'X' : 'O');
+            if (winner.isDraw) {
+                status = 'Draw';
+            } else {
+                status = "Next player: " + (this.state.xIsNext ? 'X' : 'O');
+            }
         }
         
         return (
@@ -160,13 +164,23 @@ function calculateWinner(squares) {
             return {
                 winner: squares[a],
                 line: lines[i],
-            }
+                isDraw: false,
+            };
+        }
+    }
+
+    let isDraw = true;
+    for (let i = 0; i < squares.length; i++) {
+        if (squares[i] == null) {
+            isDraw = false;
+            break;
         }
     }
 
     return {
         winner: null,
         line: null,
+        isDraw: isDraw,
     };
 
 }
